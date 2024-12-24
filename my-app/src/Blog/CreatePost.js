9544e7697ed02d14d209c34687a52ecd0360ef42
Blew_Dreamer <103670494+amshunaik@users.noreply.github.com>
2024-12-24 23:14:26 +0530

    Update CreatePost.js

--- my-app/src/Blog/CreatePost.js
+++ my-app/src/Blog/CreatePost.js
@@ -13,7 +13,7 @@ const CreatePost = () => {
       alert("Fill complete details to create a post");
       return;
     }
-    await axios.post('https://blogpost-fxut.onrender.com/posts', { title, content });
+    await axios.post('https://blogpost-ir8s.onrender.com/posts', { title, content });
     navigate('/');
   };
 
