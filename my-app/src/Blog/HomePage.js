bdc8e7282a4a96df7179276300ba154b5d29fe80
Blew_Dreamer <103670494+amshunaik@users.noreply.github.com>
2024-12-24 23:47:33 +0530

    Update HomePage.js

--- my-app/src/Blog/HomePage.js
+++ my-app/src/Blog/HomePage.js
@@ -8,7 +8,7 @@ const HomePage = () => {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await axios.get('https://blogpost-ir8s.onrender.com/posts');
+        const response = await axios.get('https://blogpost1-nxy5.onrender.com/posts');
         const posts = response.data; // Extracting the array
         setPosts(posts)
        console.log(posts);
